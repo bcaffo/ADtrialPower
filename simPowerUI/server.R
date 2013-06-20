f3be2271cf10933878fcb6783a8418232af42def
Brian Caffo <bcaffo@gmail.com>
2013-06-19 20:40:09 -0400

    Worked on server.R

--- simPowerUI/server.R
+++ simPowerUI/server.R
@@ -1,9 +1,14 @@
 library(shiny)
 library(lme4)
 library(mvtnorm)
+source("simPower.R")
 
 shinyServer(
     function(input, output) {  
+        
+        output$mtime <- renderPrint({
+            seq(from = 0, to = input$yrs, length = input$m)            
+        })
         
     }
 )
--- simPowerUI/ui.R
+++ simPowerUI/ui.R
@@ -6,7 +6,7 @@ shinyUI(
         sidebarPanel(
             h4("Enter simulation settings, the defaults are estimated from our analysis of ADNI"),
             numericInput("beta1", "Yearly increase in CDR SB for diseased group", 0.924, min = 0, max = 2, step = .001),
-            numericInput("perd", "Percent decrease in the incline in CDR SB for treated group expressed as a proportion", 0.30, min = 0.01, max = 1.0, step = .01 ),
+            numericInput("perd", "Percent decrease in the incline in CDR SB for treated group expressed as a proportion (example to consider a 30% decrease, going from a 1.0 mean yearly increase in CDR SB to 0.7, enter 0.30)", 0.30, min = 0.01, max = 1.0, step = .01 ),
             numericInput("sigma0", "Random intercept standard deviation (remember to express as sd)", 0.762, min = 0, max = 2, step = .01),
             numericInput("sigma1", "Random slope standard deviation (remember to express as a sd)", 0.844, min = 0, max = 2, step = .01),
             numericInput("sigma", "Error standard deviation (remember to express as a sd)", 0.628, min = 0.1, max = 2, step = .1),
@@ -15,21 +15,16 @@ shinyUI(
                         list("Random intercept and slope" = "rirs", 
                              "Random slope only" = "rs", 
                              "Random intercept only" = "ri")),
-            checkboxGroupInput("mtime", "Visit times in years, select all that apply", 
-                           c("Baseline" = 0,
-                             ".5 Years (6 Months)" = .5,
-                             "1.0 Year" = 1,
-                             "1.5 Years" = 1.5,
-                             "2.0 Years" = 2.0,
-                             "2.5 Years" = 2.5,
-                             "3.0 Years" = 3.0,
-                             "3.5 Years" = 3.5),
-            ),
-            numericInput("nosim", "Number of simulations to run. Use at least 1,000 for final results", 100, min = 100, step = 100),
+            numericInput("m", "Number of equally spaced visits, 2 for only baseline and followup", 3, min = 2, step = 1),
+            numericInput("yrs", "Total length of trial in years", 1, min = .5, step = .5),
+            numericInput("nosim", "Number of simulations to run. Use 10 to test things out, 100 for preliminary results and at least 1,000 for final results", 10, min = 100, step = 100),
             submitButton("Submit")
         ),
         mainPanel(
-            h4('Results of simulation')
+            h3('Results of simulation'),
+            h4('Your trial has visit times in years'),
+            verbatimTextOutput("summary")
+            
           )
     )
 )
